57da3831705088cd2a82bf391d85de555a2c5040
SUPRATIK BHATTACHARYA <42605643+Grtpersonality@users.noreply.github.com>
2019-08-16 22:45:27 +0530

    Add files via upload

---
+++ Supratik_Bhattacharya/code4.py
@@ -0,0 +1,9 @@
+P=(int(input("Enter the Principal amount:  ")))
+R=(int(input("Enter the rate of interest:  ")))
+T=(int(input("Enter the time: ")
+
+SI=(P*R*T)/100
+n=(int(input("Enter number of times:")))
+CI=P(1+R/n)^nT
+print("the simple interest is ",SI)
+print("the compound interest is",CI)
