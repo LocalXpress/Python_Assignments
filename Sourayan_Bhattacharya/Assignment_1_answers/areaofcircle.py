f7fd017d256ec8b0689ca10765b7e8b4c27840ce
sourayanbh <53990781+sourayanbh@users.noreply.github.com>
2019-08-17 09:11:57 +0530

    Add files via upload

---
+++ Sourayan_Bhattacharya/Assignment_1_answers/areaofcircle.py
@@ -0,0 +1,3 @@
+rad=int(input("Enter the Radius of The Circle : "))
+area=((22/7)*(rad*rad))
+print("Area of the The Circle  is: ",area)
---
+++ Sourayan_Bhattacharya/Assignment_1_answers/swapnum.py
@@ -0,0 +1,3 @@
+a=int(input("Enter The First number: "))
+b=int(input("Enter The Second number: "))
+c=int(input("Enter The Third Number: "))
