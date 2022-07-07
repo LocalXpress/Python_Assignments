dc76a81892ab4526e56ae3fe331a884797d260ee
SUPRATIK BHATTACHARYA <42605643+Grtpersonality@users.noreply.github.com>
2022-07-07 18:04:45 +0530

    Update Program_1.py

--- Day 1/Program_1.py
+++ Day 1/Program_1.py
@@ -1,6 +1,6 @@
 a = int(input("Enter the value of a: "))
 b = int(input("Enter the value of b: "))
+c = int(input("Enter the value of c: "))
+d = a+b+c
 
-c = a+b
-
-print("The value of c is: ",c)
+print("The result is: ",d)
